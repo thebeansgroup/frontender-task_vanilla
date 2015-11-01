2b2f7dc1fdd444ffdb6ebd6481897ea060424425
JavaCakez <tom.hargrave19@gmail.com>
2015-11-01 16:18:48 +0000

    Solution - Tom Hargrave

--- src/components/comments/index.js
+++ src/components/comments/index.js
@@ -1,3 +1,5 @@
+import {Comment, Image, Name, Donation, Message} from './elements.js';
+
 export default class {
   constructor(data) {
     this.data = data;
@@ -6,9 +8,25 @@ export default class {
   }
 
   createFactories() {
+	  this.comments = new Array();
+	  for (var i = 0; i < this.data.comments.length; i++) {
+		  this.comments.push(new Comment(this.data.comments[i]))
+	  }
   }
 
   render() {
-    return "comments";
+    this.renderOutput = `
+		<ul class="comments">
+    `;
+	
+	for (var i = 0; i < this.comments.length; i++) {
+		this.renderOutput = this.renderOutput.concat(this.comments[i].render());
+	}
+
+	this.renderOutput = this.renderOutput.concat(`
+		</ul>
+    `);
+
+	return this.renderOutput;
   }
 }
