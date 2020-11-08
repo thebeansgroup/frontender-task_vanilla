c96149fb9afe18edf67a674423786bd7b1ee9751
Gregor Cox <gregor@fxdigital.uk>
2020-11-08 11:22:09 +0000

    feat: add and display comment heading component

--- src/components/comments/elements.js
+++ src/components/comments/elements.js
@@ -1,22 +1,56 @@
+/**
+ * Heading Component
+ */
+
+export class Heading {
+    constructor(name, donation) {
+        this.name = name;
+        this.donation = donation;
+    }
+
+    render() {
+        const formatDonation = (donation) => {
+            let donationString = '';
+
+            // Only show donation info if the value is greater than 0
+            if (donation > 0) {
+                // Adds a decimal place to the value
+                donationString = `donated £${(donation / 100).toFixed(2)}`
+            }
+            return donationString;
+        }
+
+        return (
+            `<p>${this.name} ${formatDonation(this.donation)}</p>`
+        );
+    }
+}
+
+
+/**
+ * Comment Component
+ */
 export class Comment {
     constructor({ name, donation, comment, children }) {
-        this.name = name;
+        this.heading = new Heading(name, donation).render();
         this.comment = comment;
 
         this.children = children.map(child => new Comment(child).render());
     }
 
     render() {
-        return `
-        <div>
-          <div">
-            <p>${this.name}</p>
-          </div>
-          <div>
-            <p>${this.comment}</p>
-          </div>
-          ${this.children}
-        </div>
-      `
+        return (
+          `
+            <div>
+              <div>
+                <div>
+                    <p>${this.heading}</p>
+                    <p>${this.comment}</p>
+                </div>
+              </div>
+              ${this.children}
+            </div>
+          `
+        );
     }
 }
