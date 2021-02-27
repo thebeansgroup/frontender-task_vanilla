2d6cf4836c3895dee7f8888a0b42cc9cd6838541
gapark <georgeanthonypark@gmail.com>
2021-02-27 11:48:09 +0000

    built comments component

--- src/components/comments/index.js
+++ src/components/comments/index.js
@@ -1,14 +1,30 @@
+
+import Comment from '../comment';
+
 export default class {
   constructor(data) {
     this.data = data;
-    this.createFactories();
     this.render();
   }
 
-  createFactories() {
+  createComments(commentsData) {
+    let comments = '';
+    commentsData.forEach((commentData) => {
+      comments += new Comment(commentData).render();
+      if (commentData.children.length) {
+        commentData.children.forEach((childCommentData) => {
+          comments += new Comment(childCommentData, true).render()
+        });
+      }
+    });
+    return comments
   }
 
   render() {
-    return "comments";
+    return `
+      <div class="column">
+        ${this.createComments(this.data.comments)}
+      <div>
+    `
   }
 }
