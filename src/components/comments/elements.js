9d41a6429e6ff01edf48054f9404e3e688da9ce2
Calum Macdonald <c@lum.sh>
2021-10-11 23:24:06 +0100

    basic comments styling

--- src/components/comments/elements.js
+++ src/components/comments/elements.js
@@ -9,30 +9,62 @@ export class Comment {
 
   render() {
     let comment = `
-      <div class="comment">
-        <img src="${this.comment.image}"/>
-        ${this.comment.name}
+      <div class="comments__comment">
+        <div class="comments__comment__content">
+          <div class="comments__comment__content__image">
+            <img src="${this.comment.image}"/>
+          </div>
+          <div class="comments__comment__content__text">
+            <span class="comments__comment__content__text__name">
+              ${this.comment.name}
+            </span>
     `;
 
     if(this.comment.donation > 0) {
       comment += `donated £${this.comment.donation/100}`
     }
 
-    comment += this.comment.comment
+    comment += `
+      <p>
+        ${this.comment.comment}
+      </p>
+    `;
 
-    comment += `</div>`
+    comment += `
+          </div>
+        </div>
+        <div class="comments__comment___tail"></div>
+      </div>
+    `;
 
     this.comment.children.forEach(element => {
       let child = `
-        <div class="child-comment">
-          <img src="${element.image}"/>
-          ${element.name}
+        <div class="comments__child-comment">
+          <div class="comments__comment__content">
+            <div class="comments__comment__content__image">
+              <img src="${element.image}"/>
+            </div>
+          <div class="comments__comment__content__text">
+            <span class="comments__comment__content__text__name">
+              ${element.name}
+            </span>
       `;
       if (element.donation > 0) {
         child += `donated £${element.donation / 100}`;
       }
 
-      child += element.comment;
+      child += `
+      <p>
+        ${element.comment}
+      </p>
+    `;
+      
+      child += `
+          </div>
+        </div>
+        <div class="comments__child-comment___tail"></div>
+      </div>
+      `;
 
       comment += child
     });
